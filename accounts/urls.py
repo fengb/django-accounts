f8bf82979a3e57788c8133885633e81bd3f34c4c
fengb <benjamin.feng@gmail.com>
2013-04-15 18:28:08 -0500

    Added password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, url
 from django.conf import settings
 
 urlpatterns = patterns('django.contrib.auth.views',
-    url(r'^login/$', 'login', {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^logout/$', 'logout', {'next_page': settings.LOGIN_URL}, name='logout'),
+    url(r'^login/$',            'login',                name='login'),
+    url(r'^logout/$',           'logout',               name='logout',
+                kwargs={'next_page': settings.LOGIN_URL}),
+    url(r'^password-change/$',  'password_change',      name='password_change'),
+    url(r'^password-changed/$', 'password_change_done', name='password_change_done'),
 )
